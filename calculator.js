8de08a6bd26f6cd8232e8345baad93950cb2002d
Andrew Tram <MyMBP@Andrews-MacBook-Pro.local>
2017-03-14 19:00:20 -1000

    finished calculator.js

--- calculator.js
+++ calculator.js
@@ -6,6 +6,25 @@
  * @return {object} `calculator` object that can be used
  */
 
+var calculatorModule = (function(){
+  var _memory = 0;
+  var _total = 0;
+  
+
+  return {
+    load: _load,
+    getTotal: getTotal,
+    add: add,
+    subtract: subtract,
+    multiply: multiply,
+    divide: divide,
+    recallMemory: recallMemory,
+    saveMemory: saveMemory,
+    clearMemory: clearMemory,
+
+  };
+
+
 
   /**
    * sets the `total` to the number passed in
@@ -13,53 +32,99 @@
    * @return { Number }    current total
    */
 
+  function _load(x){
+    if(typeof x !== "number"){
+    throw new error( "wrong input, please try again");
+   }
+    _total = x;
 
+
+    return _total;
+  } 
+
+
+ 
   /**
    * Return the value of `total`
    * @return { Number }
    */
 
+  function getTotal(){
+    return _total;
+  }
 
   /**
    * Sums the value passed in with `total`
    * @param { Number } x
    */
 
+   function add(x){
+    if(typeof x !== "number"){
+    throw new error( "wrong input, please try again");
+    }
+      _total += x;
+   }
+   
 
   /**
    * Subtracts the value passed in from `total`
    * @param  { Number } x
    */
 
+   function subtract(x){
+    if(typeof x !== "number"){
+    throw new error( "wrong input, please try again");
+    }
+      _total -= x;
+   }
 
   /**
    * Multiplies the value by `total`
    * @param  { Number } x
    */
-
+   function multiply(x){
+    if(typeof x !== "number"){
+    throw new error( "wrong input, please try again");
+    }
+      _total *= x;
+   }
 
   /**
    * Divides the value passing in by `total`
    * @param  { Number } x
    */
-
+   function divide(x){
+    if(typeof x !== "number"){
+    throw new error( "wrong input, please try again");
+    }
+      _total /= x;
+   }
 
   /**
    * Return the value stored at `memory`
    * @return { Number }
    */
-
+   function recallMemory(){
+      return _memory;
+   }
 
   /**
    * Stores the value of `total` to `memory`
    */
-
+   function saveMemory(){
+      _memory = _total;
+      
+   }
 
   /**
    * Clear the value stored at `memory`
    */
+   function clearMemory(){
+      _memory = 0;
+   }
 
   /**
    * Validation
    */
 
+});
